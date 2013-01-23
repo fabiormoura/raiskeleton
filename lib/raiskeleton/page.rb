aa6743ec9f9564df5cad8c22743f1e7b52fa2f44
Fabio Maia <fabioromm@gmail.com>
2013-01-22 22:10:11 -0300

    Add tests to page class

--- lib/raiskeleton/page.rb
+++ lib/raiskeleton/page.rb
@@ -2,21 +2,20 @@ module Raiskeleton
   class Page
     attr_accessor :action
     attr_accessor :sections
-    attr_accessor :default_layout
+#    attr_accessor :default_layout
 
     def initialize(action)
-      raise "action should not be blank" if action.blank?
+      raise "action cannot be nil or empty" if action.nil? || action.empty?
+
       self.action = action
       self.sections = {}
     end
 
-    def update_section(section_name,&block)
-      raise "section name should not be blank" if section_name.blank?
-      unless self.sections.has_key?(section_name.to_s)
-        self.sections[section_name.to_s] = Raiskeleton::Section.new(section_name)
+    def update_section(name, &block)
+      if not sections.has_key?(name)
+        sections[name] = Raiskeleton::Section.new(name)
       end
-      section = self.sections[section_name.to_s]
-      block.call(section)
+      block.call(sections[name]) if block_given?
     end
   end
 end
---
+++ test/unit/raiskeleton/page_test.rb
@@ -0,0 +1,73 @@
+require 'shoulda'
+require 'mocha'
+require 'page'
+
+module Raiskeleton
+  class PageTest < Test::Unit::TestCase
+    context "action" do
+      should "create a with given action" do
+        action = stub(:empty? => false)
+        page = Raiskeleton::Page.new action
+        assert_equal action, page.action
+      end
+
+      should "raise an exception when action is nil" do
+        assert_raise(RuntimeError) do
+          Raiskeleton::Page.new nil
+        end
+      end
+
+      should "raise an exception when action is empty" do
+        assert_raise(RuntimeError) do
+          Raiskeleton::Page.new ""
+        end
+      end
+    end
+
+    context "sections" do
+      setup do
+        name = stub(:empty? => false)
+        @page = Raiskeleton::Page.new name
+      end
+
+      should "create a page with no sections" do
+        assert @page.sections.empty?
+      end
+
+      should "create a section with given name" do
+        name = stub
+        section = stub
+
+        Raiskeleton::Section.stubs(:name_valid?).returns(true)
+        Raiskeleton::Section.expects(:new).with(name).returns(section)
+
+        @page.update_section(name)
+
+        assert @page.sections.key?(name)
+        assert_equal section, @page.sections[name]
+      end
+
+      should "not create a new section when name already exists" do
+        name = stub
+        section = stub
+        Raiskeleton::Section.stubs(:name_valid?).returns(true)
+        Raiskeleton::Section.expects(:new).with(name).returns(section).once
+
+        @page.update_section(name)
+        @page.update_section(name)
+        assert_equal 1, @page.sections.keys.length
+      end
+
+      should "call block when updating section" do
+        name = stub
+        section = stub
+        Raiskeleton::Section.stubs(:name_valid?).returns(true)
+        Raiskeleton::Section::expects(:new).with(name).returns(section)
+        block = Proc.new { }
+        block.expects(:call).with(section)
+
+        @page.update_section(name,&block)
+      end
+    end
+  end
+end
