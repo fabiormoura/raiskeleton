222feeee3c627b327c442d7addb8ac7c7e4dede9
Fabio Maia <fabioromm@gmail.com>
2013-01-27 15:27:00 -0300

    Add tests to skeleton class

--- lib/raiskeleton/skeleton.rb
+++ lib/raiskeleton/skeleton.rb
@@ -1,5 +1,6 @@
 require 'raiskeleton/group'
 require 'raiskeleton/pages'
+
 module Raiskeleton
   class Skeleton
     attr_accessor :groups
@@ -10,28 +11,32 @@ module Raiskeleton
       self.pages = {}
     end
 
-    def add_group(group_name,&block)
-      group = Raiskeleton::Group.new(group_name)
-      raise "group #{group.name} was already specified" if self.groups.has_key?(group.name.to_s)
-      block.call(group)
-      self.groups[group.name.to_s] = group
+    def register_group(name,&block)
+      if not groups.has_key?(name)
+        groups[name] = Raiskeleton::Group.new(name)
+      else
+        raise "group #{name} was already registered"
+      end
+      groups[name].instance_eval(&block) if block_given?
     end
 
-    def get_group(group_name)
-      raise "group #{group_name} does not exist" unless self.groups.has_key?(group_name.to_s)
-      self.groups[group_name.to_s]
+#    def get_group(group_name)
+#      raise "group #{group_name} does not exist" unless self.groups.has_key?(group_name.to_s)
+#      self.groups[group_name.to_s]
+#    end
+
+    def register_pages(name,&block)
+      if not pages.has_key?(name)
+        pages[name] = Raiskeleton::Pages.new(name)
+      else
+        raise "pages #{name} was already registered"
+      end
+      pages[name].instance_eval(&block) if block_given?
     end
 
-    def add_pages(pages_name,&block)
-      pages = Raiskeleton::Pages.new(pages_name)
-      raise "pages #{pages.name} was already specified" if self.pages.has_key?(pages.name.to_s)
-      block.call(pages)
-      self.pages[pages.name.to_s] = pages
-    end
-
-    def get_pages(pages_name)
-      raise "pages #{pages_name} does not exist" unless self.pages.has_key?(pages_name.to_s)
-      self.pages[pages_name.to_s]
-    end
+#    def get_pages(pages_name)
+#      raise "pages #{pages_name} does not exist" unless self.pages.has_key?(pages_name.to_s)
+#      self.pages[pages_name.to_s]
+#    end
   end
 end
---
+++ test/unit/raiskeleton/skeleton_test.rb
@@ -0,0 +1,91 @@
+require 'test/unit'
+require 'shoulda'
+require 'mocha'
+require 'skeleton'
+
+module Raiskeleton
+  class SkeletonTest < Test::Unit::TestCase
+    context "initialize" do
+      should "create skeleton with empty groups" do
+        skeleton = Raiskeleton::Skeleton.new
+        assert skeleton.groups.empty?
+      end
+
+      should "create skeleton with empty pages" do
+        skeleton = Raiskeleton::Skeleton.new
+        assert skeleton.pages.empty?
+      end
+    end
+
+    context "register_group" do
+      setup do
+        @skeleton = Raiskeleton::Skeleton.new
+      end
+
+      should "register group with given name" do
+        name = stub
+        group = stub
+        Raiskeleton::Group.stubs(:new).with(name).returns(group)
+        @skeleton.register_group(name)
+
+        assert_equal group, @skeleton.groups[name]
+      end
+
+      should "raise an exception when group already exists" do
+        name = stub
+        Raiskeleton::Group.stubs(:new).with(name).returns(stub).once
+
+        @skeleton.register_group(name)
+        assert_raise(RuntimeError) do
+          @skeleton.register_group(name)
+        end
+      end
+
+      should "call block when registering a group" do
+        name = stub(:empty? => false)
+        group = stub
+        group.expects(:instance_eval)
+        Raiskeleton::Group.expects(:new).with(name).returns(group)
+        block = Proc.new { }
+
+        @skeleton.register_group(name,&block)
+      end
+    end
+
+    context "register_pages" do
+      setup do
+        @skeleton = Raiskeleton::Skeleton.new
+      end
+
+      should "register pages object with given name" do
+        name = stub
+        page = stub
+        Raiskeleton::Pages.stubs(:new).with(name).returns(page)
+        @skeleton.register_pages(name)
+
+        assert_equal page, @skeleton.pages[name]
+      end
+
+      should "raise an exception when pages object already exists" do
+        name = stub
+        Raiskeleton::Pages.stubs(:new).with(name).returns(stub).once
+
+        @skeleton.register_pages(name)
+        assert_raise(RuntimeError) do
+          @skeleton.register_pages(name)
+        end
+      end
+
+      should "call block when registering a pages object" do
+        name = stub(:empty? => false)
+        pages = stub
+        pages.expects(:instance_eval)
+        Raiskeleton::Pages.expects(:new).with(name).returns(pages)
+        block = Proc.new { }
+
+        @skeleton.register_pages(name,&block)
+      end
+    end
+  end
+end
+
